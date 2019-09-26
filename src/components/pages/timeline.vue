b2baa1c21e54631e25d84c59683ba7407c781086
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-09-26 22:08:29 +0900

    [add]timelineを追加

---
+++ src/components/pages/timeline.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <v-card v-for="timeline in timelines" :key="timeline.id">
+            <v-card-title>
+                
+            </v-card-title>
+        </v-card>
+    </v-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
